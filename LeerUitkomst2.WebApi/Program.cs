8bf6bfc9382a6744aca05cb1a78462b983e9a724
Martijn <oldmartijntje@gmail.com>
2025-02-20 15:24:32 +0100

    new homepeag :3
    meow

--- LeerUitkomst2.WebApi/Program.cs
+++ LeerUitkomst2.WebApi/Program.cs
@@ -1,4 +1,7 @@
 using ProjectMap.WebApi.Repositories;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,15 +35,38 @@ if (sqlConnectionStringNotFound)
 {
     imageUrl = "https://i.imgur.com/I77WgZI.jpeg";
 }
-app.MapGet("/", () => Results.Content(@$" <!DOCTYPE html>
-<html>
-    <head>
-        <title>Hehe haw = {sqlConnectionStringNotFound}</title>
-    </head>
-    <body>
-        <img src=""{imageUrl}"">
-    </body>
-</html> ", "text/html"));
+app.MapGet("/", () => Results.Content($@"<!DOCTYPE html>
+<html lang = ""en"">
+<head>
+  <meta charset = ""UTF-8"">
+  <meta name = ""viewport"" content = ""width=device-width, initial-scale=1.0"">
+  <title> Random {(sqlConnectionStringNotFound ? "Dog" : "Cat")} </title>
+  <style>
+    html, body {{
+margin: 0;
+padding: 0;
+height: 100 %;
+    background - color: #000; /* optional: a dark background */
+    }}
+# catImage {{
+width: 100 %;
+height: 100 %;
+object-fit: cover;
+display: block;
+    }}
+  </style>
+</head>
+<body>
+  <img id=""catImage"" src=""https://cataas.com/cat/says/click%20meh%20%3A3"" alt=""Random Cat"">
+  
+  <script>
+    const catImage = document.getElementById('catImage');
+catImage.addEventListener('click', () => {{
+    catImage.src = 'https://cataas.com/cat?' + new Date().getTime();
+}});
+  </script>
+</body>
+</html>", "text/html"));
 
 app.UseHttpsRedirection();
 
